2a791d224b4ec37bf213af09967a543bad6bbcbd
Fatemehesk <fatemeh@brandweb.agency>
2024-03-19 12:23:43 +0200

    Adding react router dom to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,36 @@
-import {Fragment,useState} from 'react';
-import Header from './Layout/Header';
-import Meals from './Meals/Meals';
-import Cart from './Cart/Cart';
-import CartProvider from './store/CartProvider';
-
+import React,{ useState } from "react";
+import Cart from "./Cart/Cart";
+import CartProvider from "./store/CartProvider";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/index";
+import FavoritMeals from "./pages/favorits/index";
+import MealsDetail from "./pages/detail/index";
+import HeaderCartButton from "./Layout/HeaderCartButton";
+import classes from './Layout/Header.module.css';
+import Navbar from "./Cart/Navbar/Navbar";
 function App() {
-  const [cartIsShown,setCartIsShown]=useState(false);
-  const showCartHandler=()=>{
-setCartIsShown(true);
-  };
-  const hideCartHandler=()=>{
-setCartIsShown(false);
-  };
+  const [cartIsShown, setCartIsShown] = useState(false);
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+};
+const showCartHandler = () => {
+  setCartIsShown(true);
+};
   return (
-<CartProvider>
- {cartIsShown && <Cart onClose={hideCartHandler}/>} 
-  <Header onShowCart={showCartHandler}/>
-  <Meals/>
-</CartProvider>
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <header className={classes.header}>
+                
+                <Navbar/>
+               <HeaderCartButton onClick={showCartHandler}/>
+            </header>
+      <Routes>
+        <Route path='/' element={<HomePage cartStatus={setCartIsShown}/>}/>
+        <Route path='/favorits' element={<FavoritMeals/>}/>
+        <Route path='/items/:id' element={<MealsDetail/>}/>
+      </Routes>
+    
+    </CartProvider>
   );
 }
 
